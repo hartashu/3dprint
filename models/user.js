b4ab8f82fd04bfcc31bd82a1fa6a2ebf8aa09b3b
hartashu <harta.shuwanto@gmail.com>
2025-06-03 16:18:05 +0700

    feat: multer success upload files

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,13 +14,81 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasOne(models.Profile);
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Username is empty!'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Username is empty!'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Email is empty!'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email is empty!'
+        },
+        isEmail: {
+          args: true,
+          msg: 'Please input correct email!'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Password is empty!'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Password is empty!'
+        }
+      }
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Select your role first!'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Select your role first!'
+        }
+      }
+    }
   }, {
+    hooks: {
+      beforeCreate: async (user, options) => {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+        
+        user.password = hash;
+      }
+    },
     sequelize,
     modelName: 'User',
   });
